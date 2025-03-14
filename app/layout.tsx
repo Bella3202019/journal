b9d89a0f7c1ea55385e84759334a4f3b1307a8e4
Bella3202019 <18916618951@163.com>
2025-03-14 15:54:21 +0800

    basic info

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,13 +8,39 @@ import { cn } from "@/utils";
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'),
   title: "Echome",
-  description: "Your emotional support partner",
+  description: "Echo Your Inner Growth",
   openGraph: {
-    images: ['/title.png'],
+    title: 'Echome',
+    description: 'Echo Your Inner Growth',
+    images: [
+      {
+        url: '/share.png',
+        width: 1920,
+        height: 1080,
+        alt: 'Echome - Echo Your Inner Growth'
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+    siteName: 'Echome'
   },
   twitter: {
-    images: ['/title.png'],
-  }
+    card: 'summary_large_image',
+    title: 'Echome',
+    description: 'Echo Your Inner Growth',
+    images: ['/share.png'],
+    creator: '@echome'
+  },
+  icons: {
+    icon: '/history.png',
+    apple: '/history.png'
+  },
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: 'Echome',
+  },
 };
 
 export default function RootLayout({
